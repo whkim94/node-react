9f14dbd412d46d537abbe152864c5d5689234b56
Jonathan Kim <whkim94@gmail.com>
2025-04-12 01:39:51 +0900

    Fix: import route

--- client/src/store/slices/authSlice.ts
+++ client/src/store/slices/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { User, AuthResponse } from '../types';
+import { User, AuthResponse } from '../../types';
 
 interface AuthState {
   token: string | null;
